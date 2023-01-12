aef4e83164632af19a9172d0c60823bc2526f8ef
fscott2017 <fscott2017@fau.edu>
2023-01-12 16:48:44 -0500

    added

--- guessGame.js
+++ guessGame.js
@@ -1,6 +1,7 @@
 var randomNumber = Math.floor(Math.random() * 10) + 1;
 var amountGuessed = 1;
 var maxGuess = 0;
+//Set max guesses for and number range for each level
 function level1() {
   randomNumber = Math.floor(Math.random() * 10) + 1;
   maxGuess = 4;
@@ -17,9 +18,11 @@ function level4() {
   randomNumber = Math.floor(Math.random() * 1000) + 1;
   maxGuess = 15;
 }
+//function to interpret user guessed value
 function userGuess() {
   var guessed = document.getElementById("guessedValue").value;
   if (maxGuess >= amountGuessed) {
+    //used to make sure user can't guess more then max guess value
     if (guessed == randomNumber) {
       document.getElementById("result").innerHTML = "Correct, You Win!";
     } else if (guessed > randomNumber) {
@@ -35,10 +38,12 @@ function userGuess() {
       "You have ran out of guesses! The answer was: " + randomNumber;
   }
 }
+//Used jQuery to keep button active
 $(".button").on("click", function () {
   $(".button").removeClass("active");
   $(this).addClass("active");
 });
+//used to refresh the page so the user can play again
 const refresh = () => {
   location.reload();
 };
